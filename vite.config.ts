0d55d28a6c63e27d3cc197026433775c0a1b7aa8
Supreme-hax <arshanulhoq@gmail.com>
2025-09-03 13:17:18 +0600

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client/src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "./client/index.html"
+    }
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
